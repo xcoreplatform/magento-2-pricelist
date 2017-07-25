cd2e4f7723199dbe9627b40c0b0a972f78ff7a15
Eelco <e.dam@linq5.nl>
2017-07-25 15:11:08 +0200

    Fixed error on foreign keys

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -22,131 +22,121 @@ class InstallSchema implements InstallSchemaInterface
 
         $table_dealer4dealer_price_list = $setup->getConnection()->newTable($setup->getTable('dealer4dealer_price_list'));
 
-        $table_dealer4dealer_price_list->addColumn(
-            'id',
-            Table::TYPE_INTEGER,
-            null,
-            ['nullable' => false, 'identity' => true, 'auto_increment' => true, 'unsigned' => true, 'primary' => true],
-            'Price List ID'
-        );
+        $table_dealer4dealer_price_list
+            ->addColumn(
+                'id',
+                Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'identity' => true, 'auto_increment' => true, 'unsigned' => true, 'primary' => true],
+                'Price List ID'
+            )
+            ->addColumn(
+                'guid',
+                Table::TYPE_TEXT,
+                36,
+                ['nullable' => false],
+                'Price List GUID'
+            )
+            ->addColumn(
+                'code',
+                Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'Price List Code'
+            )
+            ->addIndex(
+                'IDX_D4D_PRICE_LIST_GUID',
+                ['guid'],
+                ['type' => 'UNIQUE']
+            )
+            ->setComment('xCore Price List Table');
 
-        $table_dealer4dealer_price_list->addColumn(
-            'guid',
-            Table::TYPE_TEXT,
-            null,
-            ['nullable' => false],
-            'Price List GUID'
-        );
-
-        $table_dealer4dealer_price_list->addColumn(
-            'code',
-            Table::TYPE_TEXT,
-            null,
-            ['nullable' => false],
-            'Price List Code'
-        );
-
-        $table_dealer4dealer_price_list->addIndex(
-            'IDX_D4D_PRICE_LIST_GUID',
-            ['guid'],
-            ['type' => 'UNIQUE']
-        );
-
+        $setup->getConnection()->createTable($table_dealer4dealer_price_list);
 
         $table_dealer4dealer_price_list_item = $setup->getConnection()->newTable($setup->getTable('dealer4dealer_price_list_item'));
 
-        $table_dealer4dealer_price_list_item->addColumn(
-            'id',
-            Table::TYPE_INTEGER,
-            null,
-            ['nullable' => false, 'identity' => true, 'auto_increment' => true, 'primary' => true, 'unsigned' => true],
-            'Price List Item ID'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'price_list_id',
-            Table::TYPE_INTEGER,
-            null,
-            ['nullable' => false],
-            'Price List ID'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'product_id',
-            Table::TYPE_INTEGER,
-            null,
-            ['nullable' => false],
-            'Price List Item Product ID'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'qty',
-            Table::TYPE_DECIMAL,
-            null,
-            [
-                'nullable'  => false,
-                'scale'     => 4,
-                'precision' => 12,
-                'default'   => 1.0000
-            ],
-            'Price List Item Quantity'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'price',
-            Table::TYPE_DECIMAL,
-            null,
-            [
-                'nullable'  => false,
-                'scale'     => 4,
-                'precision' => 12,
-                'default'   => 0.0000
-            ],
-            'Price List Item Price'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'start_date',
-            Table::TYPE_DATETIME,
-            null,
-            [],
-            'Price List Item Start Date'
-        );
-
-        $table_dealer4dealer_price_list_item->addColumn(
-            'end_date',
-            Table::TYPE_DATETIME,
-            null,
-            [],
-            'Price List Item End Date'
-        );
-
-        $table_dealer4dealer_price_list_item->addIndex(
-            'IDX_D4D_PRICE_LIST_ID_PRODUCT_ID_QTY',
-            ['price_list_id', 'product_id', 'qty'],
-            ['type' => 'UNIQUE']
-        );
-
-        $table_dealer4dealer_price_list_item->addForeignKey(
-            'FK_D4D_PRICE_LIST_ID',
-            'price_list_id',
-            $installer->getTable('dealer4dealer_price_list'),
-            'id',
-            Table::ACTION_CASCADE
-        );
-
-
-        $table_dealer4dealer_price_list_item->addForeignKey(
-            'FK_PRODUCT_ID',
-            'product_id',
-            $installer->getTable('catalog_product_entity'),
-            'entity_id',
-            Table::ACTION_CASCADE
-        );
+        $table_dealer4dealer_price_list_item
+            ->addColumn(
+                'id',
+                Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'identity' => true, 'auto_increment' => true, 'primary' => true, 'unsigned' => true],
+                'Price List Item ID'
+            )
+            ->addColumn(
+                'price_list_id',
+                Table::TYPE_INTEGER,
+                10,
+                ['nullable' => false, 'unsigned' => true],
+                'Price List ID'
+            )
+            ->addColumn(
+                'product_id',
+                Table::TYPE_INTEGER,
+                10,
+                ['nullable' => false, 'unsigned' => true],
+                'Price List Item Product ID'
+            )
+            ->addColumn(
+                'qty',
+                Table::TYPE_DECIMAL,
+                null,
+                [
+                    'nullable'  => false,
+                    'scale'     => 4,
+                    'precision' => 12,
+                    'default'   => 1.0000
+                ],
+                'Price List Item Quantity'
+            )
+            ->addColumn(
+                'price',
+                Table::TYPE_DECIMAL,
+                null,
+                [
+                    'nullable'  => false,
+                    'scale'     => 4,
+                    'precision' => 12,
+                    'default'   => 0.0000
+                ],
+                'Price List Item Price'
+            )
+            ->addColumn(
+                'start_date',
+                Table::TYPE_DATETIME,
+                null,
+                [],
+                'Price List Item Start Date'
+            )
+            ->addColumn(
+                'end_date',
+                Table::TYPE_DATETIME,
+                null,
+                [],
+                'Price List Item End Date'
+            )
+            ->addIndex(
+                'IDX_PRICE_LIST_ID_PRODUCT_ID_QTY',
+                ['price_list_id', 'product_id', 'qty'],
+                ['type' => 'UNIQUE']
+            )
+            ->addForeignKey(
+                'FK_PRICE_LIST_ID',
+                'price_list_id',
+                'dealer4dealer_price_list',
+                'id',
+                Table::ACTION_CASCADE
+            )
+            ->addForeignKey(
+                'FK_PRODUCT_ID',
+                'product_id',
+                'catalog_product_entity',
+                'entity_id',
+                Table::ACTION_CASCADE
+            )
+            ->setComment('xCore Price List Item Table');
 
         $setup->getConnection()->createTable($table_dealer4dealer_price_list_item);
-
-        $setup->getConnection()->createTable($table_dealer4dealer_price_list);
 
         $setup->endSetup();
     }
